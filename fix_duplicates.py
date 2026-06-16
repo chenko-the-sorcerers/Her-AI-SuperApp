9291e2431982a9ccac1bd9cb773831f0ba543ca0
Faiz-Abdurrachman <faizabdurrachman2024@gmail.com>
2026-06-16 22:17:49 +0700

    chore: clean up repo by ignoring scripts and extra pdfs

--- fix_duplicates.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('/home/faiz/her/Her-AI-SuperApp/js/frontend/profile.js', 'r') as f:
-    content = f.read()
-
-# I will find all instances of function renderParticipantModules and only keep the first one
-# Actually, it's safer to just replace all of them with nothing, and then add one at the end?
-# Let's just fix the last one which is at line 970
-
